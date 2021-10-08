891f6d399ed55c1abf750bb96aa792b90ca88676
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-10-08 07:49:46 -0400

    Add files via upload

---
+++ src/Password.java
@@ -0,0 +1,21 @@
+public class Password
+{
+    public static void main(String[] args)
+    {
+        // Prompt the user to enter their password and pass their string
+        // to the passwordCheck method to determine if it is valid.
+    }
+    
+    public static boolean passwordCheck(String password)
+    {
+        // Create this method so that it checks to see that the password
+        // is at least 8 characters long and only contains letters 
+        // and numbers.
+        String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String digits = "0123456789";
+        
+        
+        
+        
+    }
+}
